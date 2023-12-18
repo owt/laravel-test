458684124dbbacbcb1299385b890bcbf7f993a43
Andrew Mason <ajm@owt.org.uk>
2023-12-18 02:21:28 +0000

    Add coffee sale model and coffee sale factory

---
+++ app/Models/CoffeeSale.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CoffeeSale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'quantity',
+        'unit_cost',
+        'selling_price',
+    ];
+
+}
