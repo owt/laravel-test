8c2c9990eb6ee6de1dc76040a85f8afdae95eb35
Andrew Mason <ajm@owt.org.uk>
2023-12-18 02:20:49 +0000

    Coffee sales migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\CoffeeSale;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Sales Agent',
             'email' => 'sales@coffee.shop',
         ]);
+
+        $coffeeSale = CoffeeSale::factory()->create([
+            'user_id' => $user->id,
+            'quantity' => 2,
+            'unit_cost' => 10.00,
+            'selling_price' => 23.33,
+        ]);
+        
     }
 }
